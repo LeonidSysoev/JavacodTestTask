f7af3f5a06434effb0da4ee332ea0ed855a135c6
LeonidHome <defenser2205@gmail.com>
2024-08-03 13:16:35 +0300

    init commit

---
+++ src/main/java/com/javacod/wallettesttask/WalletTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.javacod.wallettesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletTestTaskApplication.class, args);
+    }
+
+}
